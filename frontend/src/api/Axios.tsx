1c7d9cdb6c76e16a230c3ff58337cccfc8e50637
SimplyUser7508 <kolya7508@gmail.com>
2024-12-24 16:09:32 +0300

    front fix

--- frontend/src/api/Axios.tsx
+++ frontend/src/api/Axios.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-    baseURL: 'https://sport-sections.up.railway.app/api',
+    baseURL: 'https://sport-sections.onrender.com/api',
 });
 
 const accessToken = localStorage.getItem('accessToken');
